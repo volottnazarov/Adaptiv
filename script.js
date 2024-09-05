b698ad6c9700b9b9f7e60246c9b35b63f853fd7a
владимир назаров <volottnazarov@mail.ru>
2024-09-06 00:14:48 +0300

    Adaptive_Burger_Menu

---
+++ script.js
@@ -0,0 +1,8 @@
+﻿const burgerMenu = document.querySelector('.burger_menu');
+const burger = document.querySelector('.butMobile_1');
+const menuClose = document.querySelector('.burger_menu-close');
+function toggleMenu() {
+    burgerMenu.classList.toggle('hidden');
+}
+burger.addEventListener('click', toggleMenu);
+menuClose.addEventListener('click', toggleMenu);
